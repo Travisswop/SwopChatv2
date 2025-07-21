efe17e270425c3524103e7d4021dcbf540b24d86
Swop Technologies INC <98964561+Travisswop@users.noreply.github.com>
2025-07-20 22:26:42 -0400

    Create page.tsx

---
+++ web/app/page.tsx
@@ -0,0 +1,14 @@
+// web/app/page.tsx
+'use client';
+
+import React from 'react';
+import Chat from '../Chat';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Swop Chat</h1>
+      <Chat />
+    </main>
+  );
+}
